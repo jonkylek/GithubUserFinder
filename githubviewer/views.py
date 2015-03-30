0142efd1b6aca8f7dcb4dcee3ced3d7635d26c81
Jon Kennedy <jonkylek@email.arizona.edu>
2015-03-30 14:16:21 -0700

    Cleaned up urls and views .py

--- githubviewer/views.py
+++ githubviewer/views.py
@@ -2,31 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    response = HttpResponse("Here's the text of the web page.")
     context = {}
     template = "index.html"
     return render(request, template, context)
-
-def results(request):
-    context = {}
-    template = "results.html"
-    return render(request, template, context)
-
-def results2(request):
-    context = {}
-    template = "results2.html"
-    return render(request, template, context)
-
 
 def githubFinder(request):
     context = {}
     template = "GithubFinder.html"
     return render(request, template, context)
-
-
-
-
-
-# def home2(request):
-#     context = {}
-#     template = "home2.html"
-#     return render(request, template, context)
